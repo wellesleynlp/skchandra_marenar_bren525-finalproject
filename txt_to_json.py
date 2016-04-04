dfcbc2984a4b09e23f554df1b151752c7d593e8c
Marena Richardson <marena.richardson@gmail.com>
2016-04-04 16:58:56 -0400

    improve regex

--- txt_to_json.py
+++ txt_to_json.py
@@ -9,29 +9,29 @@ def update_dict(results, regex, text, date):
     regexp = re.compile(regex)
     people = set()
     for phrase in regexp.findall(text):
-    	cleaned = re.split('[:;]', phrase)[0]
-    	people.add(cleaned)
+    	people.add(phrase)
     for person in people:
-    	if person not in results:
-    		results[person] = {}
-    	results[person][date] = []
+        cleaned = re.split('[:;]', person.split('\n')[1])[0]
+    	if cleaned not in results:
+    		results[cleaned] = {}
+    	results[cleaned][date] = []
     r = re.split(regex, text)
     for i, item in enumerate(r):
-    	item_cleaned = re.split('[:;]', item)[0]
-    	if item_cleaned in people:
-            print item_cleaned
+    	if item in people:
+            item_cleaned = re.split('[:;]', item.split('\n')[1])[0]
             results[item_cleaned][date].append(r[i+1])
     return results
 
 def main(datadir):
-	files = os.listdir(datadir)
-	results = {}
-	for filename in files: 
-		f = codecs.open(datadir + "/" + filename, 'r', encoding='utf-8')
-		text = f.read()
-		date = filename.split('.')[0]
-		results = update_dict(results, r'([A-Z][A-Z]*[. \n]*[A-Z]*[:;])', text, date)
-	
+    files = os.listdir(datadir)
+    results = {}
+    for filename in files: 
+        f = codecs.open(datadir + "/" + filename, 'r', encoding='utf-8')
+        text = f.read()
+        date = filename.split('.')[0]
+        results = update_dict(results, r'([\n][A-Z][A-Z]*[a-z]*[. \n]*[A-Z]*[a-z]*[A-Z]*[:;])', text, date)
+    print results
+
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
 	parser.add_argument("datadir", help="directory where dataset is located", type=str)
