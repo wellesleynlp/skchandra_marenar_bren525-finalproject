486e4c5b83d0638137bbdb701b1d3e9a7b3708ff
Marena Richardson <marena.richardson@gmail.com>
2016-04-06 20:44:46 -0400

    remove punctuation

--- parse.py
+++ parse.py
@@ -5,15 +5,14 @@ import os
 import itertools
 from collections import defaultdict
 from nltk import word_tokenize, sent_tokenize
-
-
+from string import punctuation
 
 def main(datadir):
 	testing = ['2016', '2008', '1976']
 	megadict = defaultdict(lambda: {})
 	for filename in os.listdir(datadir):
 		year = filename.split('_')[0]
-		f = open(datadir + '/' + filename, 'r')
+		f = codecs.open(datadir + '/' + filename, 'r', encoding='utf-8')
 		data = json.load(f)
 		f.close()
 		if year not in testing:
@@ -23,8 +22,10 @@ def main(datadir):
 					words = [word_tokenize(t) for t in sentences]
 					for index, w in enumerate(words):
 						words[index] = [x.lower() for x in w]
-					megadict[name][date] = words
-	newf = open(datadir + '/parsed.json' , 'w')
+					exclude = set(punctuation)
+					s = [word for sentence in words for word in sentence if word not in exclude]
+					megadict[name][date] = s
+	newf = codecs.open(datadir + '/parsed.json' , 'w', encoding='utf-8')
 	newf.write(json.dumps(megadict))
 	newf.close()
 
