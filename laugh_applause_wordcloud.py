ca809685512b3f2f6a486db94d93504d6fd64b2e
Kiki <shivali.chandra@students.olin.edu>
2016-05-13 03:50:38 -0400

    graphs of highest avg counts

--- laugh_applause_wordcloud.py
+++ laugh_applause_wordcloud.py
@@ -4,12 +4,14 @@ from scipy.misc import imread
 import codecs, json
 from collections import defaultdict
 import string
+import numpy as np
+import seaborn
 
 stop = ["a", "about", "above", "above", "across", "after", "afterwards", "again", "against", "all", "almost", "alone", "along", "already", "also","although","always","am","among", "amongst", "amoungst", "amount",  "an", "and", "another", "any","anyhow","anyone","anything","anyway", "anywhere", "are", "around", "as",  "at", "back","be","became", "because","become","becomes", "becoming", "been", "before", "beforehand", "behind", "being", "below", "beside", "besides", "between", "beyond", "bill", "both", "bottom","but", "by", "call", "can", "cannot", "cant", "co", "con", "could", "couldnt", "cry", "de", "describe", "detail", "do", "done", "down", "due", "during", "each", "eg", "eight", "either", "eleven","else", "elsewhere", "empty", "enough", "etc", "even", "ever", "every", "everyone", "everything", "everywhere", "except", "few", "fifteen", "fify", "fill", "find", "fire", "first", "five", "for", "former", "formerly", "forty", "found", "four", "from", "front", "full", "further", "get", "give", "go", "had", "has", "hasnt", "have", "he", "hence", "her", "here", "hereafter", "hereby", "herein", "hereupon", "hers", "herself", "him", "himself", "his", "how", "however", "hundred", "ie", "if", "in", "inc", "indeed", "interest", "into", "is", "it", "its", "itself", "keep", "last", "latter", "latterly", "least", "less", "ltd", "made", "many", "may", "me", "meanwhile", "might", "mill", "mine", "more", "moreover", "most", "mostly", "move", "much", "must", "my", "myself", "name", "namely", "neither", "never", "nevertheless", "next", "nine", "no", "nobody", "none", "noone", "nor", "not", "nothing", "now", "nowhere", "of", "off", "often", "on", "once", "one", "only", "onto", "or", "other", "others", "otherwise", "our", "ours", "ourselves", "out", "over", "own","part", "per", "perhaps", "please", "put", "rather", "re", "same", "see", "seem", "seemed", "seeming", "seems", "serious", "several", "she", "should", "show", "side", "since", "sincere", "six", "sixty", "so", "some", "somehow", "someone", "something", "sometime", "sometimes", "somewhere", "still", "such", "system", "take", "ten", "than", "that", "the", "their", "them", "themselves", "then", "thence", "there", "thereafter", "thereby", "therefore", "therein", "thereupon", "these", "they", "thickv", "thin", "third", "this", "those", "though", "three", "through", "throughout", "thru", "thus", "to", "together", "too", "top", "toward", "towards", "twelve", "twenty", "two", "un", "under", "until", "up", "upon", "us", "very", "via", "was", "we", "well", "were", "what", "whatever", "when", "whence", "whenever", "where", "whereafter", "whereas", "whereby", "wherein", "whereupon", "wherever", "whether", "which", "while", "whither", "who", "whoever", "whole", "whom", "whose", "why", "will", "with", "within", "without", "would", "yet", "you", "your", "yours", "yourself", "yourselves", "the","going","thats","know","think","dont","say","im","i","want","hes","like","didnt","ill","just","youre","id","ive","theres","theyre"]
 
 wordcloud = WordCloud(max_font_size=100, relative_scaling=.5,background_color='white')
 
-def plot(name,freq):
+def plot_cloud(name,freq):
 	shape = imread('america.jpg')
 	wordcloud.mask = shape
 
@@ -36,9 +38,45 @@ def get_freq(name):
 					if fin_word not in stop:
 						freq[fin_word] += 1
 
-	plot(name,freq.items())
+	plot_cloud(name,freq.items())
 
-get_freq('laughter')
-get_freq('applause')
+def plot_graph(name,val):
+	y = np.arange(len(val))
+	width = 0.25
 
+	x = [v[1] for v in val]
+	names = [v[0] for v in val]
 
+	plt.figure(figsize=(15,12))
+
+	p1 = plt.barh(y,x)
+	p2 = plt.yticks(y+width,names,fontsize=12)
+	plt.title('Top Candidates for Highest Average Counts of '+name.title(),fontsize=16)
+	plt.xlabel('Average Count Per Debate',fontsize=16)
+	plt.savefig(name+'_freq.png')
+	plt.close()
+	
+def get_top(name):
+	newf = codecs.open(name+'_count.json','r',encoding='utf-8')
+	f = json.load(newf)
+
+	short_names = defaultdict(int)
+	for key,val in f.iteritems():
+		person = key.split(' ')
+		year = person[-1]
+		n = ' '.join(person[:-2])+' \''+year[-2:]
+		short_names[n] += val
+
+	top = sorted(filter(lambda x: short_names[x] > 0, short_names.keys()), key=short_names.get, reverse=True)
+
+	plot_vals = []
+	for person in top:
+		plot_vals.append([person,short_names[person]])
+	
+	plot_graph(name,plot_vals)
+
+#get_freq('laughter')
+#get_freq('applause')
+
+print get_top('laughter')
+print get_top('applause')
